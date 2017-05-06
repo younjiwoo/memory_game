43664c2217a939b3365c94015a416077d8ea1d36
younjiwoo <younjiwoo@gmail.com>
2017-05-06 14:32:06 -0700

    initial commit

--- js/main.js
+++ js/main.js
@@ -2,12 +2,9 @@
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-var cardOne = cards[0];
-cardsInPlay.push(cardOne);
-console.log("User flipped " + cardOne);
-var cardTwo = cards[2];
-cardsInPlay.push(cardTwo);
-console.log("User flipped " + cardTwo);
+
+var flipCard = function(cardId) {
+	
 if (cardsInPlay.length === 2) {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		alert("You found a match!");
@@ -15,3 +12,5 @@ if (cardsInPlay.length === 2) {
 		alert("Sorry, try again.");
 	}
 }
+}
+
