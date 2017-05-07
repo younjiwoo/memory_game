3c3c3a744f3609c40290361d59a0905997cd9178
younjiwoo <younjiwoo@gmail.com>
2017-05-06 21:44:57 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -2,15 +2,21 @@
 
 var cards = ["queen", "queen", "king", "king"];
 var cardsInPlay = [];
-
-var flipCard = function(cardId) {
-	
-if (cardsInPlay.length === 2) {
+var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
-		alert("You found a match!");
+		console.log("You found a match!");
 	} else {
-		alert("Sorry, try again.");
+		console.log("Sorry, try again.")
 	}
 }
+
+var flipCard = function(cardId) {
+	console.log("User flipped " + cards[cardId]);
+	cardsInPlay.push(cards[cardId]);
+
+	checkForMatch();
 }
+
+flipCard(0);
+flipCard(2);
 
