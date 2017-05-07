2787b2b0d7f2077f935e47f11d4747665e9fb611
younjiwoo <younjiwoo@gmail.com>
2017-05-06 22:52:17 -0700

    created game logic

--- js/main.js
+++ js/main.js
@@ -1,19 +1,42 @@
 // alert('Hello, friends.');
 
-var cards = ["queen", "queen", "king", "king"];
+var cards = [
+	{
+		"rank": "queen",
+		"suit": "hearts",
+		"cardImage": "images/queen-of-hearts.png"
+	},
+	{
+		"rank": "queen",
+		"suit": "diamonds",
+		"cardImage": "images/queen-of-diamonds.png"
+	},
+	{
+		"rank": "king",
+		"suit": "hearts",
+		"cardImage": "images/king-of-hearts.png"
+	},
+	{
+		"rank": "king",
+		"suit": "diamonds",
+		"cardImage": "images/king-of-diamonds.png"
+	}
+];
 var cardsInPlay = [];
 var checkForMatch = function() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
 		console.log("You found a match!");
 	} else {
-		console.log("Sorry, try again.")
+		console.log("Sorry, try again.");
 	}
 }
 
 var flipCard = function(cardId) {
-	console.log("User flipped " + cards[cardId]);
-	cardsInPlay.push(cards[cardId]);
+	console.log("User flipped " + cards[cardId].rank);
+	cardsInPlay.push(cards[cardId].suit);
 
+	console.log(cards[cardId].cardImage);
+	console.log(cards[cardId].suit);
 	checkForMatch();
 }
 
